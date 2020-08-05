6a4a30b17b322e03e6f5a77063d1451f56157adc
root <root@comnetsemu>
2020-08-05 13:45:49 +0000

    Push with connection errors in mqqt.js

--- webserver/app.js
+++ webserver/app.js
@@ -15,39 +15,49 @@ var app = express();
 var mqttHandler = require('./public/javascripts/mqtt');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 var mqttClient = new mqttHandler();
 mqttClient.connect();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/weather', usersRouter);
+//app.use('/weather', usersRouter);
 //app.use('/sendMqtt', mqttRouter);
-
-// catch 404 and forward to error handler
+app.get('/weather', function(req, res, next) {
+        console.log('router' + mqttClient.json_log);
+        res.render('weather', {
+            title: 'weather',
+            message: (mqttClient.json_log)
+        });
+    })
+    // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- webserver/public/javascripts/mqtt.js
+++ webserver/public/javascripts/mqtt.js
@@ -1,52 +1,99 @@
 const mqtt = require('mqtt');
+const fs = require('fs')
 var requestify = require('requestify');
 
+
+
 class MqttHandler {
-  constructor() {
-    this.mqttClient = null;
-    this.host = 'http://10.0.0.1:1883';
 
-    this.info_track_weather = []        // List of dictionaries {'position': kml, 'weather': json_weatherbit.io}
-  }
-  
-  connect() {
-    
-    this.mqttClient = mqtt.connect(this.host);
+    constructor() {
+        this.mqttClient = null;
+        this.host = 'http://10.0.0.1:1883';
 
-    // Mqtt error calback
-    this.mqttClient.on('error', (err) => {
-      console.log(err);
-      this.mqttClient.end();
-    });
-
-    // Connection callback
-    this.mqttClient.on('connect', () => {
-      console.log(`mqtt client connected`);
-    });
-
-    // mqtt subscriptions
-    this.mqttClient.subscribe('sensor/gps', {qos: 0});        // Subscribe to drone GPS position
-
-    // When a message arrives, console.log it
-    this.mqttClient.on('message', function (topic, message) {
-      console.log(topic + ": " + message.toString());
-      
-      if (topic == 'sensor/gps') {
-        // Call to json_weatherbit.io 
+        this.info_track_weather = [] // List of dictionaries {'position': kml, 'weather': json_weatherbit.io}
+        this.json_log = [];
         
-        // this.info_track_weather.push({'position': tokml(message), 'weather': w})
-      }
-    });
+    }
 
-    this.mqttClient.on('close', () => {
-      console.log(`mqtt client disconnected`);
-    });
-  }
+    connect() {
 
-  // Sends a mqtt message to topic: mytopic
-  sendMessage(message) {
-    this.mqttClient.publish('mytopic', message);
-  }
+        
+
+        this.mqttClient = mqtt.connect(this.host);
+
+        // Mqtt error calback
+        this.mqttClient.on('error', (err) => {
+            console.log(err);
+            this.mqttClient.end();
+        });
+
+        // Connection callback
+        this.mqttClient.on('connect', () => {
+            console.log(`mqtt client connected`);
+        });
+
+        // mqtt subscriptions
+        this.mqttClient.subscribe('sensor/gps', {
+            qos: 0
+        }); // Subscribe to drone GPS position
+
+        // When a message arrives, console.log it
+        this.mqttClient.on('message', function(topic, message) {
+
+
+            this.json_log.push((topic + ": " + message.toString()));
+
+            console.log(this.json_log.length)
+
+           
+
+            
+            /*
+
+            if (topic == 'sensor/gps') {
+                // Call to json_weatherbit.io
+                // this.info_track_weather.push({'position': tokml(message), 'weather': w})
+                /*
+                requestify.get('https://api.weatherbit.io/v2.0/current?city=Trento,TN&key=245ea69ec9a14549ae68a8c0371e5eb4').then(function(response) {
+                  var weather_log =[];
+                  var weather_json = JSON.parse(response.body)
+                  weather_log.push((weather_json.data[0].temp));
+                  console.log(weather_log)
+
+                })
+                
+
+                const fs = require('fs');
+
+                // create a JSON object
+                const sensor_drone = {
+                    json_log
+                };
+
+                // convert JSON object to string
+                const data = JSON.stringify(sensor_drone);
+
+                // write JSON string to a file
+                fs.writeFile('user.json', data, (err) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log("JSON data is saved.");
+                });
+
+            }
+            */
+        });
+
+        this.mqttClient.on('close', () => {
+            console.log(`mqtt client disconnected`);
+        });
+    }
+
+    // Sends a mqtt message to topic: mytopic
+    sendMessage(message) {
+        this.mqttClient.publish('mytopic', message);
+    }
 }
 
 module.exports = MqttHandler;
